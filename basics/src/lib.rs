1f658deab14d56412bfd015de2bcd7e9e2ea6ac9
Vivek-Murali <vivekmurali00@outlook.com>
2024-05-20 19:42:38 +0530

    Completed Error Handling

--- basics/src/lib.rs
+++ basics/src/lib.rs
@@ -1,3 +1,8 @@
+use core::f64;
+use std::fs::File;
+use std::io::{Error, Read};
+use std::path::PathBuf;
+
 pub const GREETING: &'static str = "Hallo, Rust library here!";
 
 pub struct Person {
@@ -18,4 +23,46 @@ pub fn build_full_name(person: &Person) -> String {
 
     full_name.push_str(&person.last);
     full_name
+}
+
+/* Safe Division with Result Struct */
+
+
+#[derive(Debug)]
+pub struct DivisionByZeroError;
+
+pub fn safe_division(number:f64,divisor:f64) -> Result<f64,DivisionByZeroError>{
+    if divisor == 0.0{
+        Err(DivisionByZeroError)
+    }else{
+        Ok(number/divisor)
+    }
+}
+
+// File operation w/d Result Strcut
+
+pub fn read_file_contents(path: PathBuf) -> Result<String, Error> {
+    let mut string = String::new();
+
+    // Access a file at a specified path
+    // ---------------------------------
+    // TODO #1:
+    // - Pass variable to `file` variable on success, or
+    // - Return from function early if there's an error
+    let mut file: File = match File::open(path) {
+        Ok(file_handle) =>file_handle,
+        Err(io_error) => return Err(io_error)
+    };
+
+    // Read file contents into `String` variable with `read_to_string`
+    // ---------------------------------
+    // Success path is already filled in
+    // TODO #2: Return from function early if there's an error
+    match file.read_to_string(&mut string) {
+        Ok(_) => &string,
+        Err(io_error) => return Err(io_error)
+    };
+
+    // TODO #3: Return `string` variable as expected by function signature
+    Ok(string)
 }
--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,4 +1,5 @@
 use basics::*;
+use std::path::PathBuf;
 
 fn main() {
     println!("Hello, world! {}",GREETING);
@@ -8,5 +9,12 @@ fn main() {
         last: String::from("Smith"),
     };
     assert_eq!(build_full_name(&john), "James Oliver Smith");
-    println!("{}",build_full_name(&john))
+    println!("{}",build_full_name(&john));
+    println!("{:?}",safe_division(45.0, 3.0));
+    if read_file_contents(PathBuf::from("data/PSD Q&A.txt")).is_ok() {
+        println!("{:?}",read_file_contents(PathBuf::from("data/PSD Q&A.txt")).unwrap());
+    }
+    if read_file_contents(PathBuf::from("non-existent-file.txt")).is_err() {
+        println!("The program reported an error for the file that doesn't exist.");
+    }
 }
